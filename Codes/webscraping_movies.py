e2e7c6ff960bd00e03d0eb973e9c26b4a85d6ae2
Saikat Pal <119132976+saikat912@users.noreply.github.com>
2026-07-08 09:10:51 +0530

    Add files via upload
    Webscraping of top 50 movies

---
+++ Codes/webscraping_movies.py
@@ -0,0 +1,108 @@
+import requests
+import sqlite3
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+url = "https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films"
+
+db_name = "Movies.db"
+table_name = "Top_50"
+csv_path = "/home/project/top_50_films.csv"
+
+df = pd.DataFrame(
+    columns=[
+        "Average Rank",
+        "Film",
+        "Year"
+    ]
+)
+
+count = 0
+
+
+"""
+Using requests.get().text function
+and parsing the HTML using BeautifulSoup
+to extract required information.
+"""
+
+
+# Extract webpage data
+
+html_page = requests.get(url).text
+
+data = BeautifulSoup(
+    html_page,
+    "html.parser"
+)
+
+
+# Find table rows
+
+tables = data.find_all("tbody")
+
+rows = tables[0].find_all("tr")
+
+
+# Extract top 50 movies
+
+for row in rows:
+
+    if count < 50:
+
+        col = row.find_all("td")
+
+        if len(col) != 0:
+
+            data_dict = {
+                "Average Rank": col[0].contents[0],
+                "Film": col[1].contents[0],
+                "Year": col[2].contents[0]
+            }
+
+            df1 = pd.DataFrame(
+                data_dict,
+                index=[0]
+            )
+
+            df = pd.concat(
+                [
+                    df,
+                    df1
+                ],
+                ignore_index=True
+            )
+
+            count += 1
+
+    else:
+        break
+
+
+# Print dataframe
+
+print(df)
+
+
+# Save dataframe as CSV
+
+df.to_csv(
+    csv_path,
+    index=False
+)
+
+
+# Save dataframe into SQLite database
+
+conn = sqlite3.connect(db_name)
+
+df.to_sql(
+    table_name,
+    conn,
+    if_exists="replace",
+    index=False
+)
+
+conn.close()
+
